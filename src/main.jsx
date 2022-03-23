2112d740cc2bed51b8cb318693994eaf47ba6649
Nagro <nagrogamesafic@gmail.com>
2022-03-23 16:55:19 -0500

    fix link

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.render(
     <Header/>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="nosotros" element={<Nosotros />} />
+        <Route path="/nosotros" element={<Nosotros />} />
       </Routes>
     <Footer/>
   </BrowserRouter>,
